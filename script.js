8def5cfaccea6c68d2de116134b35d886e612e11
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-08 20:53:27 -0500

    Change to prompt messages, allow restarting game

--- script.js
+++ script.js
@@ -37,13 +37,15 @@ function playRound(playerSelection, computerSelection){
 
 function game(){
     let playerScore = 0;
-
+    let nextPrompt="";
     for(i = 0; i < 5; i++){
-        let playerChoice = prompt("rock/paper/scissors, your choice, your opponent is the computer. This is round " + (i+1) + " out of 5.").toLowerCase();
+
+        let playerChoice = prompt(nextPrompt + "\nrock/paper/scissors, your choice, your opponent is the computer. This is round " + (i+1) + " out of 5.").toLowerCase();
+        nextPrompt ="";
         let computerChoice = getComputerChoice();
         if(playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors"){
             playerChoice = "rock";
-            console.log("You didn't pick one of the options so I picked rock for you. Everyone picks rock.");
+            nextPrompt += "You didn't pick one of the options so I picked rock for you. Everyone picks rock.\n";
         }
 
         let result = playRound(playerChoice, computerChoice);
@@ -53,38 +55,46 @@ function game(){
             playerScore+= result;
 
         }
-    
-        if (playerChoice == computerChoice){
-            console.log("You tied, you both picked " + computerChoice + "! We won't count this as one of the five rounds.");
-        } else if (playerChoice == "rock"){
-            if(computerChoice == "paper"){
-                console.log("The computer picked paper and smothered your rock. You lose!");
-            } else {
-                console.log("You crushed the computer's scissors with a rock, you win!");
-            }
-        } else if (playerChoice == "paper"){
-            if(computerChoice == "rock"){
-                console.log("You smothered the computer's rock with your paper. You win!");
-            } else {
-                console.log("The computer snipped up your paper with scissors. You lose!");
-            }
-        } else{
-            if(computerChoice == "rock"){
-                console.log("The computer bashed your scissors into dust with a rock. You lose!");
-            } else {
-                console.log("You sliced and diced the paper's paper with your scissors. You win!");
-            }
-        }
 
-        console.log("The score is now you: "+ playerScore + " to the computer: " + (i+1-playerScore));
+        nextPrompt += calcResultText(playerChoice, computerChoice);
+
+        nextPrompt += "\nThe score is now you: "+ playerScore + " to the computer: " + (i+1-playerScore);
     }
 
-    console.log("The FINAL score is you: " + playerScore + " to computer: " + (5-playerScore));
+    nextPrompt =  "The FINAL score is you: " + playerScore + " to computer: " + (5-playerScore);
     if(playerScore >= 3){
-        console.log("Congratulations you beat the computer in this random game of chance.");
+        nextPrompt += "\nCongratulations you beat the computer in this random game of chance.";
     } else {
-        console.log("The computer won this random game of chance. Surely it wasn't rigged...");
+        nextPrompt += "\nThe computer won this random game of chance. Surely it wasn't rigged...";
+    }
+    nextPrompt +="\nDo you want to play again?";
+    if(confirm(nextPrompt)){
+        game();
     }
 }
 
+
+function calcResultText(playerChoice, computerChoice){
+    if (playerChoice == computerChoice){
+        return "You tied, you both picked " + computerChoice + "! We won't count this as one of the five rounds.";
+    } else if (playerChoice == "rock"){
+        if(computerChoice == "paper"){
+            return "The computer picked paper and smothered your rock. You lose!";
+        } else {
+            return "You crushed the computer's scissors with a rock, you win!";
+        }
+    } else if (playerChoice == "paper"){
+        if(computerChoice == "rock"){
+            return "You smothered the computer's rock with your paper. You win!";
+        } else {
+            return "The computer snipped up your paper with scissors. You lose!";
+        }
+    } else{
+        if(computerChoice == "rock"){
+            return "The computer bashed your scissors into dust with a rock. You lose!";
+        } else {
+            return "You sliced and diced the paper's paper with your scissors. You win!";
+        }
+    }
+}
 game();
